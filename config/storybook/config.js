fda9dd4bedeca780d70f5f153a0984b6513f5a23
Pavel <avedomaru@gmail.com>
2019-04-09 12:25:40 +0300

    add custom styles to storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from '@storybook/vue'
 
+import '../../src/styles/styles.scss'
+
 const req = require.context('../../src/stories', true, /.stories.js$/)
 
 function loadStories () {
